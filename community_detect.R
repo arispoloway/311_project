4a10975eaf3c59b72e2f1aa5754a32af053bb168
Aris Poloway <agpoloway@gmail.com>
2018-12-01 17:01:25 -0500

    Basic community detection

--- community_detect.R
+++ community_detect.R
@@ -1,6 +1,7 @@
 library(bipartite);
-year <- "2010"
-t <- read.table(paste("./programming/networks/311/", year, ".txt", sep=""));
-b <- bipartite::DIRT_LPA_wb_plus(do.call(rbind, t));
-write(b$Row_labels, paste("./programming/networks/311/r_processing/", year, "_rows.txt", sep=""))
-write(b$Col_labels, paste("./programming/networks/311/r_processing/", year, "_cols.txt", sep=""))
+for (year in 2010:2016){
+  t <- read.table(paste("./programming/networks/311/", year, ".txt", sep=""));
+  b <- bipartite::DIRT_LPA_wb_plus(do.call(rbind, t), mini=4, reps=25);
+  write(b$Row_labels, paste("./programming/networks/311/r_processing/", year, "_cts.txt", sep=""), ncolumns=500)
+  write(b$Col_labels, paste("./programming/networks/311/r_processing/", year, "_reasons.txt", sep=""), ncolumns=500)
+}
