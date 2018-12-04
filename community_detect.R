66fa6981c0e333f493a3170d2d21bdf26f209a95
Aris Poloway <agpoloway@gmail.com>
2018-12-04 11:36:54 -0500

    Make code nicer

--- community_detect.R
+++ community_detect.R
@@ -1,7 +1,9 @@
 library(bipartite);
 for (year in 2010:2016){
-  t <- read.table(paste("./programming/networks/311/", year, ".txt", sep=""));
-  b <- bipartite::DIRT_LPA_wb_plus(do.call(rbind, t), mini=4, reps=25);
-  write(b$Row_labels, paste("./programming/networks/311/r_processing/", year, "_cts.txt", sep=""), ncolumns=500)
-  write(b$Col_labels, paste("./programming/networks/311/r_processing/", year, "_reasons.txt", sep=""), ncolumns=500)
+  t <- read.table(paste("./", year, ".txt", sep=""));
+  b <- bipartite::DIRT_LPA_wb_plus(do.call(rbind, t), mini=2);
+  write(b$Row_labels, paste("./r_processing/", year, "_cts.txt", sep=""), ncolumns=500)
+  write(b$Col_labels, paste("./r_processing/", year, "_reasons.txt", sep=""), ncolumns=500)
+  print(year)
 }
+
