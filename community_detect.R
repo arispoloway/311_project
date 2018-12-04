00253e916fb24c7a327b5aa1396de7a90d51f3e6
Aris Poloway <agpoloway@gmail.com>
2018-12-04 15:15:50 -0500

    Fix incorrect normalization

--- community_detect.R
+++ community_detect.R
@@ -1,7 +1,7 @@
 library(bipartite);
 for (year in 2010:2016){
   t <- read.table(paste("./", year, ".txt", sep=""));
-  b <- bipartite::DIRT_LPA_wb_plus(do.call(rbind, t), mini=2);
+  b <- bipartite::DIRT_LPA_wb_plus(do.call(rbind, t));
   write(b$Row_labels, paste("./r_processing/", year, "_cts.txt", sep=""), ncolumns=500)
   write(b$Col_labels, paste("./r_processing/", year, "_reasons.txt", sep=""), ncolumns=500)
   print(year)
