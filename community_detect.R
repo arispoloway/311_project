8be908cf13049c3cea704a55c79dea93ea7fb74e
Aris Poloway <agpoloway@gmail.com>
2018-12-05 18:32:29 -0500

    Clean up and add utility csv exports

--- community_detect.R
+++ community_detect.R
@@ -1,6 +1,6 @@
 library(bipartite);
 for (year in 2010:2016){
-  t <- read.table(paste("./", year, ".txt", sep=""));
+  t <- read.table(paste("./graphs/", year, ".txt", sep=""));
   b <- bipartite::DIRT_LPA_wb_plus(do.call(rbind, t));
   write(b$Row_labels, paste("./r_processing/", year, "_cts.txt", sep=""), ncolumns=500)
   write(b$Col_labels, paste("./r_processing/", year, "_reasons.txt", sep=""), ncolumns=500)
